4e4836e9686c76133b4136f4ce1e9fc917ad2e8c
vanya <vanya.moo@gmail.com>
2024-11-14 14:43:29 +0000

    Initial Commit

---
+++ KtchnPal/KtchnPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  KtchnPalApp.swift
+//  KtchnPal
+//
+//  Created by Vanya Mutafchieva on 14/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct KtchnPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
