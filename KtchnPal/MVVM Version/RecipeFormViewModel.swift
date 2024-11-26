f6fc78e51e7f4e38210b213a80b9003856bf8114
vanya <vanya.moo@gmail.com>
2024-11-26 14:44:56 +0000

    Add RecipeFormViewModel (for MVVM section)

---
+++ KtchnPal/MVVM Version/RecipeFormViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  RecipeFormViewModel.swift
+//  KtchnPal
+//
+//  Created by Vanya Mutafchieva on 26/11/2024.
+//
+
+import SwiftUI
+
+@Observable
+class RecipeFormViewModel {
+    var recipe: Recipe
+    var focus: RecipeFormView.Field?
+    
+    init(focus: RecipeFormView.Field? = .title, recipe: Recipe) {
+        self.focus = focus
+        self.recipe = recipe
+        
+        if recipe.ingredients.isEmpty {
+            self.recipe.ingredients.append(Ingredient(id: Ingredient.ID(UUID()), name: ""))
+        }
+        if recipe.instructions.isEmpty {
+            self.recipe.instructions.append(Instruction(id: Instruction.ID(UUID()), text: ""))
+        }
+    }
+    
+    func addIngredientButtonTapped() {
+        let ingredient = Ingredient(id: Ingredient.ID(UUID()), name: "")
+        recipe.ingredients.append(ingredient)
+        focus = .ingredient(ingredient.id)
+    }
+    
+    func deleteIngrediends(atOffsets indices: IndexSet) {
+        recipe.ingredients.remove(atOffsets: indices)
+        if recipe.ingredients.isEmpty {
+            recipe.ingredients.append(Ingredient(id: Ingredient.ID(UUID()), name: ""))
+        }
+        let index = min(indices.first!, recipe.ingredients.count - 1)
+        focus = .ingredient(recipe.ingredients[index].id)
+    }
+}
